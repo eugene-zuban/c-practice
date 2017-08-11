627ae6cfdc2cf0d0a1a9e21a8f94d26bfd620ba7
Jack Zuban <jack.zuban@gmail.com>
2017-08-11 01:57:29 -0500

    simple int formatting

--- c1/hello.c
+++ c1/hello.c
@@ -2,7 +2,12 @@
 
 int main (void)
 {
-		printf ("Programming is cool.\n");
+		int value1, value2, sum;
+
+		value1 = 10;
+		value2 = 20;
+		sum = value1 + value2;
+		printf ("The sum of %i and %i is %#X\n", value1, value2, sum);
 
 		return 0;
 }
