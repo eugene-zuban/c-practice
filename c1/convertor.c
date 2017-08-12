128081de4bf4c109d1e9bfe593114d5d6a9f67a4
Jack Zuban <jack.zuban@gmail.com>
2017-08-11 23:51:40 -0500

    F to C fixed conversion

---
+++ c1/convertor.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main (void)
+{
+		int 		F = 27;
+		float		C;
+
+		C = (F - 32) / 1.8;
+
+		printf("%i in Fahrenheit (F) is %f in Celsius (C)\n", F, C);
+		
+		return 0;
+}
