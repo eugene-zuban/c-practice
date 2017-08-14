d062099502ea9cca159d560a3c34859bc59c7e93
Jack Zuban <jack.zuban@gmail.com>
2017-08-14 01:38:48 -0500

    square root

---
+++ c1/square-root.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+float absoluteValue(float value)
+{
+    return (value > 0) ? value : -value;
+}
+
+float squareRoot(float number)
+{
+    const   float   epsilon = .00001;
+    float           guess = 1.0;
+
+    while (absoluteValue(guess * guess - number) >= epsilon) {
+        guess = (number / guess + guess) / 2.0;
+    }
+
+    return guess;
+}
+
+int main(void)
+{
+    float inputNumber;
+    int counter = 0;
+
+    printf("Enter your number: ");
+    scanf("%f", &inputNumber);
+
+    if (inputNumber == (float) 0) {
+        printf("Please enter number != 0\n");
+
+        return 0;
+    }
+
+    printf("square root %f = %f\n", inputNumber, squareRoot(inputNumber));
+
+    return 0;
+}
