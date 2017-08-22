db42d93bfd39997a44d80ede5db94ad7bee4fca6
Jack Zuban <jack.zuban@gmail.com>
2017-08-22 01:10:12 -0500

    URLfy: replace spaces with '%20'

---
+++ URLfy/URLfy/main.c
@@ -0,0 +1,58 @@
+//
+//  main.c
+//  URLfy: replace spaces with '%20'
+//
+//  Created by Jack Zuban on 8/21/17.
+//  Copyright © 2017 Jack Zuban. All rights reserved.
+//
+
+#include <stdio.h>
+
+void readLine(char *buffer)
+{
+    do {
+        *(buffer++) = getchar();
+    } while(*(buffer - 1) != '\n');
+
+    *(--buffer) = '\0';
+}
+
+void replaceSpaces(char dataString[])
+{
+    int origStrLen = 0, newStrLen = 0, numberOfSpaces = 0;
+
+    for (origStrLen = 0; dataString[origStrLen] != '\0'; origStrLen++) {
+        if (dataString[origStrLen] == ' ') {
+            numberOfSpaces++;
+        }
+    }
+
+    newStrLen = origStrLen + numberOfSpaces * 2;
+    dataString[newStrLen] = '\0';
+
+    for (int i = origStrLen - 1; i >= 0; i--) {
+        if (dataString[i] == ' ') {
+            dataString[--newStrLen] = '0';
+            dataString[--newStrLen] = '2';
+            dataString[--newStrLen] = '%';
+
+        } else {
+            dataString[--newStrLen] = dataString[i];
+        }
+    }
+}
+
+int main(int argc, const char * argv[]) {
+    void readLine(char *dataString);
+    void replaceSpaces(char *dataString);
+
+    char dataString[150] = {};
+
+    printf("Please enter your string: ");
+    readLine(dataString);
+
+    replaceSpaces(dataString);
+    printf("Resulting string is %s\n", dataString);
+
+    return 0;
+}
