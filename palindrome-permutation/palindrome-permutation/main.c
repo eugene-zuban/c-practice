2540c2851943f793b0c63864cd425ed86ec2d8c4
Jack Zuban <jack.zuban@gmail.com>
2017-08-26 03:34:28 -0500

    remove filterString function & just take only English ASCII form the input

--- palindrome-permutation/palindrome-permutation/main.c
+++ palindrome-permutation/palindrome-permutation/main.c
@@ -8,45 +8,34 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+// false for a non ASCII English letter.
 bool isEnglishLetter(char character)
 {
     return ((character >= 'a' && character <= 'z') || (character >= 'A' && character <= 'Z'));
 }
 
-void filterSrting(char *originalString)
-{
-    bool isEnglishLetter(char);
-    char *cleanString;
-
-    cleanString = originalString;
-
-    while(*originalString && *originalString != '\n') {
-        if (isEnglishLetter(*originalString)) {
-            *(cleanString++) = *originalString;
-        }
-
-        originalString++;
-    }
-
-    *cleanString = '\0';
-}
-
+// substruct 1 from bitVector and bit ANDing bitVector needs to return 0 if only a one bit is set.
 bool checkThatOnlyOneBitIsSet(int bitVector)
 {
     return (bitVector & (bitVector - 1)) == 0;
 }
 
+// bitVector indicates odd or even count of each phrase's character by toggling the ith bit.
 int createBitVector(char *phrase)
 {
+    bool isEnglishLetter(char letter);
     int mask, bitVector = 0;
 
     while(*phrase) {
-        mask = 1 << *phrase; // ex: 1 << 3 = 0100;
+        if (isEnglishLetter(*phrase)) {
+            mask = 1 << *phrase; // ex: 1 << 3 = 0100;
 
-        if ((bitVector & mask) == 0) { // ex. 0000 & 0100 = 0000. TRUE means that the bit represents a new character for bitVector
-            bitVector |= mask; // TRUE case: set the bit from the mask
-        } else {
-            bitVector &= ~mask; // it's a toggle for the bitVector's bit using the mask.
+            if ((bitVector & mask) == 0) { // ex. 0000 & 0100 = 0000. TRUE means that the bit represents a new character for bitVector
+                bitVector |= mask; // TRUE case: set the bit from the mask
+            } else {
+                bitVector &= ~mask; // it's a toggle for the bitVector's bit using the mask.
+            }
+
         }
 
         phrase++;
@@ -58,15 +47,13 @@ int createBitVector(char *phrase)
 // a palindrome needs to have odd number of similar characters or only one character without a pair.
 // we form an integer bit vector where each bit is a character's number ('a' is bit 97)
 // and we toggle a character's bit every time when we see the character in the string.
-bool isPermutationOfPalindrome(char *string)
+bool isPermutationOfPalindrome(char *phrase)
 {
-    void filterSrting(char *);
     bool chackThatOnlyOneBitIsSet(int);
     int createBitVector(char *);
     int bitVector;
 
-    filterSrting(string);
-    bitVector = createBitVector(string);
+    bitVector = createBitVector(phrase);
 
     // bitVector equals 0 means that we have the even number of similar characters and the string is "balanced"
     // if bitVector has more than one bit set, means that we have at least 2 odd characters that don't have their pairs on the string
