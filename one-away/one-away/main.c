07f1e7b96076c87151292930b7e7ac82fdc7e796
Jack Zuban <jack.zuban@gmail.com>
2017-08-27 23:37:38 -0500

    check that only one insert or remove

--- one-away/one-away/main.c
+++ one-away/one-away/main.c
@@ -8,37 +8,66 @@
 #include <string.h>
 #include <stdbool.h>
 
-bool cheakThatOnlyOneReplace(char *str1, char *str2) {
-    bool editFlag = false;
+// go through all the characters and check them till second difference or the end of the string
+bool checkThatOnlyOneReplace(char *str1, char *str2) {
+    bool diffFlag = false;
 
-    while (*str1) {
+    while (*str1 && *str2) {
         if (*(str1++) != *(str2++)) {
-            if (editFlag) { // if true, means that we already have one replase
+            if (diffFlag) { // if true, means that we already have one replace
                 return false;
             }
 
-            editFlag = true;
+            diffFlag = true;
         }
     }
 
-    return true; // no or only one replace
+    return true; // none or only one replace
+}
+
+// the logic is almost identical to checkThatOnlyOneReplace() function, and they could be merged into a one function
+// but we use a separate function just for illustrative purpose.
+// str1 needs to be longer than str2.
+bool checkThatOnlyOneInsertOrRemove(char *str1, char *str2)
+{
+    bool diffFlag = false;
+
+    while (*str1 && *str2) {
+        if (*(str1++) != *(str2++)) {
+            if (diffFlag) {
+                return false;
+            }
+
+            diffFlag = true;
+            str1++; // move the pointer to the next character only on str1 (it is longer, so we need to "adjust" indexes)
+        }
+    }
+
+    return true;
 }
 
 bool isOneEditAway(char *str1, char *str2)
 {
-    bool cheakThatOnlyOneReplace(char *str1, char *str2);
+    bool checkThatOnlyOneReplace(char *str1, char *str2);
+    bool checkThatOnlyOneInsertOrRemove(char *str1, char *str2);
     unsigned long int strLen1, strLen2;
 
     strLen1 = strlen(str1);
     strLen2 = strlen(str2);
 
-    if (abs(strLen1 - strLen2) > 1) { // more than 2 edits
-        return false;
-    } else if (strLen1 == strLen2) { // equal length means check for replace only
-        return cheakThatOnlyOneReplace(str1, str2);
+    if (strLen1 == strLen2) { // equal length means check for replace only
+        return checkThatOnlyOneReplace(str1, str2);
     }
 
-    return true;
+    if ((strLen1 - 1) == strLen2) { // first string is longer by 1 character
+        return checkThatOnlyOneInsertOrRemove(str1, str2);
+    }
+
+    if ((strLen1 + 1) == strLen2) { // second string is longer by one character
+        return checkThatOnlyOneInsertOrRemove(str2, str1);
+    }
+
+    return false; // strings lengths difference more than 1 character, so they can't be one edit away
 }
 
 int main(void) {
