c4be575deb8feab4d1765d4057a1395a98f74840
Jack Zuban <jack.zuban@gmail.com>
2017-09-02 23:58:27 -0500

    filling & printing the array

--- matrix-rotation/matrix-rotation/main.c
+++ matrix-rotation/matrix-rotation/main.c
@@ -4,30 +4,46 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
 
-void fillMatrix(int rows, int cols, int matrix[rows][cols]) {
+void fillMatrix(int rows, int cols, int matrixx[rows][cols]) {
     int col, row;
 
     for (row = 0; row < rows; ++row) {
         for (col = 0; col < cols; ++col) {
-            matrix[row][col] = 10 * (row + 1) + (col);
+            matrixx[row][col] = 10 * (col + 1) + (row + 1);
         }
     } 
 };
 
+void printMatrix(int rows, int cols, int matrix[rows][cols]) {
+    int row, col;
+
+    for (row = 0; row < rows; ++row) {
+        for (col = 0; col < cols; ++col) {
+            printf("%5i", matrix[row][col]);
+        }
+
+        printf("\n");
+    }
+};
+
 int main(void) {
     void fillMatrix(int rows, int cols, int matrix[rows][cols]);
+    void printMatrix(int rows, int cols, int matrix[rows][cols]);
 
     int mRows = 0, mCols = 0;
     
     printf("Please enter number of rows: ");
-    scanf("%i", &mRows);
+    scanf("%d", &mRows);
     printf("Please enter number of columns: ");
-    scanf("%i", &mCols);
-    
-    int matrix[5][5];
+    scanf("%d", &mCols);
+
+    int matrix[mCols][mRows];
 
     fillMatrix(mRows, mCols, matrix);
+    
+    printMatrix(mRows, mCols, matrix);
 
     return 0;
 }
