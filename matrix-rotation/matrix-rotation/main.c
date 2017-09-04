7bcbd18c5db9a13e493169d53a76386f817b89b1
Jack Zuban <jack.zuban@gmail.com>
2017-09-04 14:56:16 -0500

    working on matrix rotation

--- matrix-rotation/matrix-rotation/main.c
+++ matrix-rotation/matrix-rotation/main.c
@@ -6,15 +6,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void fillMatrix(int rows, int cols, int matrixx[rows][cols]) {
+void fillMatrix(int rows, int cols, int matrix[rows][cols]) {
     int col, row;
 
     for (row = 0; row < rows; ++row) {
         for (col = 0; col < cols; ++col) {
-            matrixx[row][col] = 10 * (row + 1) + (col + 1);
+            matrix[row][col] = 10 * (row + 1) + (col + 1);
         }
     } 
 };
+
+void rotateMatrix(int rows, int cols, int matrix[rows][cols]) {
+    int layers = rows / 2;
+
+    for (int layer = 0; layer <= layers; layer++) {
+        int first = layer;
+        int last = cols - layers;
+
+        for (int i = first; i < last; i++) {
+            int offset = last - i;
+
+            // save top
+            int top = matrix[first][i];
+
+            // left -> top
+            matrix[first][i] = matrix[offset][first];
+
+            // bottom -> left
+            matrix[offset][first] = matrix[last][offset];
+
+            // right -> bottom
+            matrix[last][offset] = matrix[i][last];
+
+            // top -> right
+            matrix[i][last] = top;
+        }
+    }
+}
 
 void printMatrix(int rows, int cols, int matrix[rows][cols]) {
     int row, col;
@@ -39,10 +67,14 @@ int main(void) {
     printf("Please enter number of columns: ");
     scanf("%d", &nCols);
 
-    int matrix[nCols][nRows];
+    int matrix[nRows][nCols];
 
     fillMatrix(nRows, nCols, matrix);
-    
+    printMatrix(nRows, nCols, matrix);
+
+    printf("\n");
+
+    rotateMatrix(nRows, nCols, matrix);
     printMatrix(nRows, nCols, matrix);
 
     return 0;
