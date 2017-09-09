468852ff5e2f3bb3043ecc8d9d11e8d636c28b59
Jack Zuban <jack.zuban@gmail.com>
2017-09-09 17:52:01 -0500

    linked-lists directory for related tasks

---
+++ linked-lists/remove-duplicates/remove-duplicates/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  remove-duplicates Remove Duplicates from a linked list.
+//
+
+#include <stdio.h>
+#include <stdbool.h>
+
+bool nodeValuesMap[52] = {}; // "hash" table for storing flags of characters from the linked list.
+
+int main(void) {
+
+    return 0;
+}
