b47c0275835f982a3fb033c53420f38299c13a2c
Jack Zuban <jack.zuban@gmail.com>
2017-09-16 17:37:27 -0500

    sum reverse lists initial commit

---
+++ linked-lists/sum-reverse-lists/sum-reverse-lists/main.c
@@ -0,0 +1,12 @@
+//
+//  main.c
+//  sum-reverse-lists: sum two linked lists represent integer numbers in reverse order.
+//  Write result to a new linked list in reverse order too.
+//  Example: (1 -> 3 -> 5) + (2 -> 3 -> 4) = (3 -> 6 -> 9), which represents 531 + 432 = 963
+//
+
+#include <stdio.h>
+
+int main(void) {
+    return 0;
+}
