26a21c35490a0d410ee312655034e6fd7a172e48
Jack Zuban <jack.zuban@gmail.com>
2017-09-17 21:25:09 -0500

    using typedef for the list definition; adding the comments; cleaning the code

--- linked-lists/sum-reverse-lists/sum-reverse-lists/main.c
+++ linked-lists/sum-reverse-lists/sum-reverse-lists/main.c
@@ -8,58 +8,61 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define LIST_END (struct node *) 0
+#define LIST_END (node *) 0
 
-struct node {
+typedef struct node {
     int data;
     struct node *next;
-};
+} node;
 
-struct node *makeList(void);
-void printList(struct node *head);
-struct node *addLists(struct node *list1, struct node *list2);
+node *makeList(void);
+void printList(node *head);
+node *addLists(node *list1, node *list2);
 
-struct node *addLists(struct node *list1, struct node *list2) {
+node *addLists(node *list1, node *list2) {
     int carry = 0, add1 = 0, add2 = 0, sum = 0;
-    struct node *result = (struct node *) malloc(sizeof(struct node));
-    struct node *head = result;
+    node *result = (node *) malloc(sizeof(node));
+    node *head = result;
 
+    // spin throught all the elements in two lists adding them digit by digit from left to right
+    // carrying their overflow to next digit(s).
     while (list1 != LIST_END || list2 != LIST_END) {
         add1 = (list1 != LIST_END) ? list1->data : 0;
         add2 = (list2 != LIST_END) ? list2->data : 0;
         sum = add1 + add2 + carry;
-        carry = sum / 10;
-        result->data = sum % 10;
+        carry = sum / 10; // will give 1 if > 10, and 0 if < 10
+        result->data = sum % 10; // take the second digit
 
+        // need this to determine do we need to create a new node for the result
         list1 = (list1 != LIST_END) ? list1->next : LIST_END;
         list2 = (list2 != LIST_END) ? list2->next : LIST_END;
-
         if (list1 != LIST_END || list2 != LIST_END) {
-            result->next = (struct node *) malloc(sizeof(struct node));
+            result->next = (node *) malloc(sizeof(node));
             result = result->next;
         }
     }
 
+    // if list1 or list2 is bigger than the other one, we need to create additional nodes for the result.
     if (carry) {
-        result->next = (struct node *) malloc(sizeof(struct node));
+        result->next = (node *) malloc(sizeof(node));
         result = result->next;
         result->data = carry;
     }
 
-    result->next = LIST_END;
+    result->next = LIST_END; // close the list.
 
     return head;
 }
 
-struct node *makeList(void) {
-    struct node *head = LIST_END, *previous = LIST_END;
+node *makeList(void) {
+    node *head = LIST_END, *previous = LIST_END;
     int items = 0, value = 0;
 
     printf("Please enter number of elements: ");
     scanf("%d", &items);
 
     while (items-- > 0) {
-        struct node *item = (struct node*) malloc(sizeof(struct node));
+        node *item = (node*) malloc(sizeof(node));
         if (item == NULL) {
             fprintf(stderr, "Memory allocation error\n");
             exit(EXIT_FAILURE);
@@ -83,9 +86,7 @@ struct node *makeList(void) {
     return head;
 }
 
-void printList(struct node *list) {
-    printf("List values: ");
-
+void printList(node *list) {
     while (list != LIST_END) {
         printf("%d ", list->data);
         list = list->next;
@@ -95,15 +96,21 @@ void printList(struct node *list) {
 }
 
 int main(void) {
-    struct node *list1 = LIST_END, *list2 = LIST_END;
+    node *list1 = LIST_END, *list2 = LIST_END;
 
+    printf("Creating List 1. ");
     list1 = makeList();
+    printf("Creating List 2 ");
     list2 = makeList();
 
+    printf("\nList 1 values: ");
     printList(list1);
+
+    printf("List 2 values: ");
     printList(list2);
 
-    struct node *sum = addLists(list1, list2);
+    node *sum = addLists(list1, list2);
+    printf("List 1 + List 2: ");
     printList(sum);
 
     return 0;
