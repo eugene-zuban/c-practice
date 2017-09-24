727421a6005905698b0e29519a4aedb60a6febbc
Jack Zuban <jack.zuban@gmail.com>
2017-09-24 14:12:33 -0500

    linked lists intersection check initial commit

---
+++ linked-lists/intersection/intersection/main.c
@@ -0,0 +1,13 @@
+//
+//  main.c
+//  intersection: find if two linked lists intersect with each other checking by nodes references not values.
+//  Return the first intersecting node.
+//
+//
+
+#include <stdio.h>
+#include <stdlinb.h>
+
+int main(void) {
+    return 0;
+}
