42c20dc992d1457c66bdcea256e1abeefde4e886
Jack Zuban <jack.zuban@gmail.com>
2017-09-24 23:31:58 -0500

    add helper for making lists merging/intersection

--- linked-lists/intersection/intersection/main.c
+++ linked-lists/intersection/intersection/main.c
@@ -20,6 +20,7 @@ node *moveHead(node *head, int steps);
 node *makeList();
 void printList(node *head);
 void printListBack(node *head);
+void makeIntersection(node *list1, node *list2, int elementsToMerge);
 
 int listLength(node *head) {
     int i = 0;
@@ -91,8 +92,30 @@ void printListBack(node *head) {
     }
 }
 
+// add intersectLength elements from the end of the list 2 to the list1 end
+void makeIntersection(node *list1, node *list2, int intersectLength) {
+    int len1 = listLength(list1), len2 = listLength(list2);
+    int offset = len2 - intersectLength;
+
+    // go the the list1 tail
+    while (list1->next != LIST_END) {
+        list1 = list1->next;
+    }
+
+    // go to the node from where we need to start the intersection
+    for (int i = 0; i < offset; i++) {
+        if (list2 != LIST_END) {
+            list2 = list2->next;
+        }
+    }
+
+    // making the intersection
+    list1->next = list2;
+}
+
 int main(void) {
     node *list1 = LIST_END, *list2 = LIST_END;
+    int intersectLength = 0;
 
     puts("Filling the first list");
     list1 = makeList();
@@ -100,16 +123,22 @@ int main(void) {
     puts("Filling the second list");
     list2 = makeList();
 
-    // adjusting the length of the lists by "cutting" the extra elements from the beginning of the list
+    printf("Please enter number of element to merge from the end of the second list to the ond of the first: ");
+    scanf("%d", &intersectLength);
+    makeIntersection(list1, list2, intersectLength);
+
+    printList(list1);
+
+    // adjusting lists length
     int length1 = listLength(list1);
     int length2 = listLength(list2);
- 
+
     if (length1 > length2) {
         list1 = moveHead(list1, length1 - length2);
     } else {
         list2 = moveHead(list2, length2 - length1);
     }
-
+    
     puts("Adjusted first list");
     printList(list1);
 
