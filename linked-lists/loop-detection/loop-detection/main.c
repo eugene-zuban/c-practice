39331774542e6e93cb6c7f6ea6b0dcde73e5625f
Jack Zuban <jack.zuban@gmail.com>
2017-09-29 02:02:21 -0500

    linked list loop detection: adding supporting functions

---
+++ linked-lists/loop-detection/loop-detection/main.c
@@ -0,0 +1,123 @@
+//
+//  main.c
+//  loop-detection: find if a given linked list has a loop and return the first node from which the loop starts.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node {
+    int data;
+    struct node *next;
+} node;
+
+#define LIST_END (node *) 0
+// main functions
+node *findLoopStart(node *head);
+node *makeList(void);
+int getlistlength(node *head);
+
+// helpers
+void makeLoop(node *head);
+void printList(node *head);
+void printListBack(node *head);
+
+node *makeList() {
+    node *head = LIST_END;
+    node *previous = LIST_END;
+    int items = 0;
+
+    printf("Please enter number of items: ");
+    scanf("%d", &items);
+
+    for (int i = 1; i <= items; i++) {
+        node *item = (node *) malloc(sizeof(node *));
+        if (item == NULL) {
+            printf("Memory allocation error\n");
+            exit(EXIT_FAILURE);
+        }
+
+        int value = 0;
+        printf("Please enter node %i value: ");
+        scanf("%d", &value);
+        item->data = value;
+        item->next = LIST_END;
+
+        if (head == LIST_END) {
+            head = item;
+        } else {
+            previous->next = item;
+        }
+
+        previous = item;
+    }
+
+    return head;
+}
+
+void printList(node *head) {
+    while (head != LIST_END) {
+        printf("%i ", head->data);
+        head = head->next;
+    }
+
+    printf("\n");
+}
+
+void printListBack(node *head) {
+    if (head != LIST_END) {
+        printListBack(head->next);
+    } else {
+        printf("\n");
+    }
+
+    printf("%i ", head->data);
+}
+
+void makeLoop(node *head) {
+    int loopStartIndex = 0;
+    int listLength = 0;
+
+    listLength = getListLength(head);
+
+    // get the node id for starting the loop
+    while (loopStartIndex == 0) {
+        printf("Please enter a node index from which the loop needs to start: ");
+        scanf("%i", &looopStartIndex);
+
+        if (loopStartIndex > listLength) {
+            printf("Loop start can't be more then list length\n");
+            loopStartIndex = 0;
+        }
+    }
+
+    node *loopStartNode = LIST_END;
+    int currentNode = 0;
+    while (head->next != LIST_END) {
+        currentNode++;
+
+        if (currentNode == loopStartIndex) {
+            loopStartNode = head;
+        }
+
+        head = head->next;
+    }
+
+    // connect the loop starting nodeto the end of the list
+    head->next = loopStartNode;
+}
+
+int getListLength(node *head) {
+    int length = 0;
+    while (head != LIST_END) {
+        length++;
+        head = head->next;
+    }
+
+    return length;
+}
+
+int main(void) {
+
+    return 0;
+}
