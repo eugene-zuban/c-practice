be89da0faf53cea7ebc0c114902b8928e4603687
Jack Zuban <jack.zuban@gmail.com>
2017-09-30 22:43:13 -0500

    rename st into arrayStack && add comments

--- stack-and-queues/stack-in-array/stack-in-array/main.c
+++ stack-and-queues/stack-in-array/stack-in-array/main.c
@@ -10,18 +10,20 @@
 typedef struct {
     unsigned int stackSize;
     unsigned int stacks;
-    int *storage;
+    int *values;
     int *sizes;
 } stack;
 
 #define STACKS_NUMBERS 3
 
+// stack-related functions
 bool push(stack *st, int stackId, int value);
 int pop(stack *st, int stackId);
 int peek(stack *st, int stackId);
 bool isEmpty(stack *st, int stackId);
 int topIndex(stack *st, int stackId);
 
+// helpers
 stack *initializeStack(void);
 void memoryAllocationError();
 
@@ -49,13 +51,13 @@ stack *initializeStack(void) {
     int stackLength = arrayStack->stacks * arrayStack->stackSize; 
  
     // allocating memory for all stacks
-    arrayStack->storage = calloc(stackLength, sizeof(int));
-    if (arrayStack->storage == NULL) {
+    arrayStack->values = calloc(stackLength, sizeof(int));
+    if (arrayStack->values == NULL) {
         memoryAllocationError();
     }
 
     for (int i = 0; i < stackLength; i++) {
-        arrayStack->storage[i] = 0;
+        arrayStack->values[i] = 0;
     }
 
     // set number of elements for each stack
@@ -64,6 +66,7 @@ stack *initializeStack(void) {
         memoryAllocationError();
     }
 
+    // set size (number of element) in each stack to 0 by default
     for (int i = 0; i < arrayStack->stacks; i++) {
         arrayStack->sizes[i] = 0;
     }
@@ -75,50 +78,62 @@ stack *initializeStack(void) {
 // stack in array operations
 //
 
-bool isEmpty(stack *st, int stackId) {
-    return st->sizes[stackId] == 0;
+bool isEmpty(stack *arrayStack, int stackId) {
+    return arrayStack->sizes[stackId] == 0;
 }
 
-bool push(stack *st, int stackId, int value) {
-    if (st->sizes[stackId] >= st->stackSize) {
+// push the value into the top of the stack specified by stackId
+bool push(stack *arrayStack, int stackId, int value) {
+    if (arrayStack->sizes[stackId] >= arrayStack->stackSize) {
         return false; // the stack is full
     }
 
-    st->sizes[stackId]++;
-    st->storage[topIndex(st, stackId)] = value;
+    // increase stack elements count
+    arrayStack->sizes[stackId]++;
+
+    // push the value into the stack
+    arrayStack->values[topIndex(arrayStack, stackId)] = value;
 
     return true;
 }
 
-int pop(stack *st, int stackId) {
-    if (isEmpty(st, stackId)) {
+// return the top value from the stack specified by stackId and shrink the stack after that
+int pop(stack *arrayStack, int stackId) {
+    if (isEmpty(arrayStack, stackId)) {
         return 0;
     }
 
-    int currentTop = topIndex(st, stackId);
-    int value = st->storage[currentTop];
-    st->storage[currentTop] = 0; // erase current value
-    st->sizes[stackId]--; //shrink the stack
+    int currentTop = topIndex(arrayStack, stackId);
+    int value = arrayStack->values[currentTop];
+
+    arrayStack->values[currentTop] = 0; // erase the current top value
+    arrayStack->sizes[stackId]--; //shrink the stack
 
     return value;
 }
 
-int peek (stack *st, int stackId) {
-    if (isEmpty(st, stackId)) {
+// return the top value from the stack specified by stackId without decreasing stack's size
+int peek (stack *arrayStack, int stackId) {
+    if (isEmpty(arrayStack, stackId)) {
         return 0;
     }
 
-    return st->storage[topIndex(st, stackId)];
+    return arrayStack->values[topIndex(arrayStack, stackId)];
 }
 
-int topIndex(stack *st, int stackId) {
-    int offset = st->stackSize * (stackId - 1);
+// return current top index for a specified stack
+int topIndex(stack *arrayStack, int stackId) {
+    int offset = arrayStack->stackSize * (stackId - 1); // the first stack has offset 0
 
-    return offset + st->sizes[stackId] - 1;
+    return offset + arrayStack->sizes[stackId] - 1; 
 }
 
 int main(void) {
     stack *arrayStack = initializeStack();
+
+    // here can go code for working with the stack
+    // example: push(arrayStack, 0, 10); for pushing value 10 to the first stack
+    // pop(arrayStack, 0); return the current top value fromt he first stack
 
     printf("Done\n");
 
