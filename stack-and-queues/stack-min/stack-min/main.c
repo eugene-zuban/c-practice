cb46611fdfd9aad8f0e14206ad1d397fd05bc99b
Jack Zuban <jack.zuban@gmail.com>
2017-10-02 20:02:49 -0500

    use global *stack pointer for working with stack

--- stack-and-queues/stack-min/stack-min/main.c
+++ stack-and-queues/stack-min/stack-min/main.c
@@ -1,38 +1,36 @@
 //
 //  main.c
-//  stack-min: stack with min() function that returns the minimum element in O(1) time
+//  stack->min: stack->with min() function that returns the minimum element in O(1) time
 //
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 
-typedef struct node {
+typedef struct stackItem {
     int value;
     int min;
-    struct node *next;
+    struct stackItem *next;
 } node;
+
+node *stack;
 
 #define STACK_END (node *) 0
 #define EMPTY_STACK_VALUE -99999
 
-bool isEmpty(node *stack);
-int min(node *stack);
-void push(node **stack, int value);
-int pop(node **stack);
-int peek(node *stack);
+bool isEmpty(void);
+int min(void);
+void push(int value);
+int pop(void);
+int peek(void);
 
 // return the current stack min value
-int min(node *stack) {
-    if (isEmpty(stack)) {
-        return EMPTY_STACK_VALUE;
-    }
-
-    return stack->min;
+int min(void) {
+    return isEmpty() ? EMPTY_STACK_VALUE : stack->min;
 }
 
 // create a new node and ad it to the stack top
-void push(node **stack, int value) {
+void push(int value) {
     node *newItem = (node *) malloc(sizeof(node));
     if (newItem == NULL) {
         printf("Memory allocation error\n");
@@ -40,68 +38,62 @@ void push(node **stack, int value) {
     }
 
     newItem->value = value;
+    newItem->next = STACK_END;
 
     // add newItem to the head
-    if (isEmpty(*stack)) {
+    if (isEmpty()) {
         newItem->min = value;
-        newItem->next = STACK_END;
     } else {
-        newItem->min = value < (*stack)->min ? value : (*stack)->min; // store new min value or use from the current stack top
-        newItem->next = *stack;
+        newItem->min = value < stack->min ? value : stack->min; // store new min value or use from the current stack top
+        newItem->next = stack;
     }
 
-    *stack = newItem;
+    stack = newItem;
 }
 
 // return the top node's value and remove the node from the stack
-int pop(node **stack) {
-    if (isEmpty(*stack)) {
+int pop(void) {
+    if (isEmpty()) {
         return EMPTY_STACK_VALUE;
     }
 
-    node *top = *stack;
-    int value = top->value;
-    *stack = top->next; // remove the top
+    int value = stack->value;
+
+    node *top = stack;
+    stack = stack->next; // move the head
     free(top); // release memory
 
     return value;
 }
 
 // return the top node without removing it
-int peek(node *stack) {
-    if (isEmpty(stack)) {
-        return EMPTY_STACK_VALUE;
-    }
-
-    return stack->value;
+int peek(void) {
+    return isEmpty() ? EMPTY_STACK_VALUE : stack->value;
 }
 
-// check if the stasck is empty
-bool isEmpty(node *stack) {
+// check if the stack is empty
+bool isEmpty() {
     return stack == STACK_END;
 }
 
 int main(void) {
-    // create a new pointer for new stack
-    node *stack = STACK_END;
-
     // push some values to our stack
-    push(&stack, 100); // stack: 100->END
-    push(&stack, 200); // stack: 200->100->END
+    push(100); // stack-> 100->END
+    push(200); // stack-> 200->100->END
 
     // test results
-    printf("Expected min value: 100, actual is: %i\n", min(stack)); // stack: 200->100->END
-    printf("Expected pop: 200, actual: %i\n", pop(&stack)); // stack: 100->END;
-    printf("Expected peek: 100, actual: %i\n", peek(stack)); // stack: 100->END;
+    printf("Expected min value: 100, actual is: %i\n", min()); // stack 200->100->END
+    printf("Expected pop: 200, actual: %i\n", pop()); // stack 100->END;
+    printf("Expected peek: 100, actual: %i\n", peek()); // stack 100->END;
 
     // set new min
-    push(&stack, 1); // stack: 1->100->END
-    printf("Expected min: 1, actual: %i\n", min(stack));
+    push(1); // stack 1->100->END
+    printf("Expected min: 1, actual: %i\n", min());
 
-    // return all the stack from stack 1->100->END
+    // return all stack values. stack: 1->100->END
     int value;
     do {
-        value = pop(&stack);
+        value = pop();
         printf("Current value: %i\n", value);
     } while (value != EMPTY_STACK_VALUE);
 
