0eea11133cefb53aa86f53cd289487c7ed423356
Jack Zuban <jack.zuban@gmail.com>
2017-10-02 20:45:35 -0500

    stack of plates initial commit

---
+++ stack-and-queues/stack-of-plates/stack-of-plates/main.c
@@ -0,0 +1,12 @@
+//
+//  main.c
+//  stack-of-plates: implement a data structure setOfStacks that includes several stacks
+//  and it should create a new stack if previous is full.
+//  pop() and push() on the setOfStacks need to work identical to a regular stack.
+//
+
+#include <stdio.h>
+
+int main(void) {
+    return 0;
+}
