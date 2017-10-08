0386abb9f4d52eec44557ac816bd6c6e6a73ca8a
Jack Zuban <jack.zuban@gmail.com>
2017-10-07 23:31:18 -0500

    dogs & cats shelter implementation

--- stack-and-queues/animal-shelter/animal-shelter/main.c
+++ stack-and-queues/animal-shelter/animal-shelter/main.c
@@ -1,17 +1,17 @@
 //
 //  main.c
-//  animal-shelter: make a FIFO equeue that accepts dogs and cats and returns the "oldest" cat or dog as requested.
+//  animal-shelter: make a FIFO queue that accepts dogs and cats and returns the "oldest" cat or dog as requested.
 //
 
 #include <stdio.h>
-#include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
 
 #define LIST_END (animalList *) 0
 
 enum animalType {dog, cat};
 
-// structure for handing an animal-related info
+// structure for handling animal-related info
 typedef struct animalNode {
     char *name;
     enum animalType type;
@@ -31,17 +31,149 @@ struct animalQueueShelter {
     int order;
 } shelter = {LIST_END, LIST_END, 0};
 
-// function declarations
-animal peek(animalList *listHead);
-animal poll(animalList **listHead);
+//
+// Linked list related functions for supporting separate lists for cats and dogs
+//
+animalList *peek(animalList *listHead);
+animal take(animalList **listHead);
 void add(animalList **listHead, animal petToAdd, int order);
 
-void enqueue(animal animalToAdd); // adds new animal to the shelter
-animal dequeueAny(void); // returns the "oldest" dog or cat fromt he shelter
-animal dequeueDog(void); // returns the "oldest" dog from the shelter
-animal dequeueCat(void); // returns the "oldest" cat from the shelter
+//
+// Queue related functions
+//
+void enqueue(animal animalToAdd); // add a new animal to the shelter
+animal dequeueAny(void); // return the "oldest" dog or cat from the shelter
+animal dequeueDog(void); // return the "oldest" dog from the shelter
+animal dequeueCat(void); // return the "oldest" cat from the shelter
 
+void testShelter(void); // ahelper for test
+
+// put pet into appropriate animalList linked list
+void enqueue(animal pet) {
+    shelter.order++;
+
+    // find where we need to add the pet
+    animalList **listToAdd;
+    if (pet.type == cat) {
+        listToAdd = &shelter.cats;
+    } else {
+        listToAdd = &shelter.dogs;
+    }
+
+    add(listToAdd, pet, shelter.order);
+}
+
+// return the "oldest" (FIFO) dog from the shelter
+animal dequeueDog(void) {
+    if (shelter.dogs == LIST_END) {
+        fprintf(stderr, "There are no dogs in the shelter.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    return take(&shelter.dogs);
+}
+
+// return the "oldest" (FIFO) cat from the shelter
+animal dequeueCat(void) {
+    if (shelter.cats == LIST_END) {
+        fprintf(stderr, "There are no cats in the shelter.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    return take(&shelter.cats);
+}
+
+// return the oldest cat or dog from the shelter
+animal dequeueAny(void) {
+    if (shelter.dogs == LIST_END && shelter.cats != LIST_END) {
+        return dequeueDog();
+    } else if (shelter.cats == LIST_END && shelter.dogs != LIST_END) {
+        return dequeueCat();
+    }
+
+    if (peek(shelter.dogs)->order > peek(shelter.cats)->order) {
+        return dequeueDog();
+    }
+
+    return dequeueCat();
+}
+
+void add(animalList **list, animal pet, int order) {
+    animalList *newItem = (animalList *) malloc(sizeof(animalList));
+
+    if (newItem == NULL) {
+        fprintf(stderr, "Malloc error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    newItem->pet = pet;
+    newItem->order = order;
+    newItem->next = LIST_END;
+
+    if (*list == LIST_END) {
+        *list = newItem;
+    } else {
+        animalList *item = *list;
+        while(item->next != LIST_END) {
+            item = item->next;
+        }
+
+        item->next = newItem;
+    }
+}
+
+// return the first (it's the oldest one) animalList node from the list
+animalList *peek(animalList *listHead) {
+    if (listHead == LIST_END) {
+        fprintf(stderr, "List is empty.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    return listHead;
+}
+
+ // take the first pet from the list (it's the oldest one)
+animal take(animalList **listHead) {
+    animal pet;
+    animalList *temp;
+
+    if (*listHead == LIST_END) {
+        fprintf(stderr, "List is empty.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // take the pet, move the head and free the old list node
+    pet = (*listHead)->pet;
+    temp = *listHead;
+    *listHead = (*listHead)->next;
+    free(temp);
+
+    return pet;
+}
+
+void testShelter(void) {
+    printf("Test shelter queue and all the supporting structires.\n");
+    
+    animal dogs[9];
+    animal cats[9];
+
+    printf("Adding a dog %s to the shelter.\n", dog1.name);
+    enqueue(dog1);
+
+    printf("Adding a dog %s to the shelter.\n", dog2.name);
+    enqueue(dog2);
+       
+    printf("Adding a dog %s to the shelter.\n", dog3.name);
+    enqueue(dog3);
+
+    printf("");
+}
 int main(void) {
+    testShelter();
+
+    printf("Take a dog. The dog is: %s\n", dequeueDog().name);
+    printf("Take a dog. The dog is: %s\n", dequeueDog().name);
+    printf("Take a dog. The dog is: %s\n", dequeueDog().name);
 
     return 0;
 }
