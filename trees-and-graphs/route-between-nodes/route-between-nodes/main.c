a35540024df27f9583eeb9f11141a417d44ab059
Jack Zuban <jack.zuban@gmail.com>
2017-10-13 02:05:31 -0500

    implementing graph and all supporting data structures

--- trees-and-graphs/route-between-nodes/route-between-nodes/main.c
+++ trees-and-graphs/route-between-nodes/route-between-nodes/main.c
@@ -9,55 +9,92 @@
 #include <stdbool.h>
 
 #define EMPTY_NODE (struct node *) 0
+#define ADJ_LIST_END (struct adjListNode *) 0
 
 enum status {unvisited, visited, visiting};
 
 // vertex node
 struct node {
-    int vertex;
+    int nodeId;
     enum status statuses;
-    struct node *next;
+
+    struct adjListNode *adjList; // holding all the edges/adjacencies
 };
 
-// adjacency list
-struct adjList {
-    struct node *head;
+// A structure for holding adjacencies (edges) of a vertex node
+struct adjListNode {
+    int destNode;
+    struct adjListNode *next;
 };
 
-// A structure to represent a directed graph. A graph is a list (array of adjacency lists)
-struct graph {
-    int vertices;
-    struct adjList *nodes;
-};
-
-// create a new node, vertex for graph
-struct node *createNode(int vertexId) {
-    struct node *newNode = (struct node *) malloc(sizeof(struct node));
-    newNode->vertex = vertexId;
-    newNode->next = EMPTY_NODE;
+// helper for creating an adjListNode
+struct adjListNode *createAdjNode(int destNodeId) {
+    struct adjListNode *newNode = (struct adjListNode *) malloc(sizeof(struct adjListNode));
+    newNode->destNode = destNodeId;
+    newNode->next = ADJ_LIST_END;
 
     return newNode;
 };
  
+// A structure to represent a directed graph. A graph is a list (array of adjacency lists)
+struct graph {
+    int vertices;
+    struct node *nodes;
+};
+
+// create a new graph with number of node = vertices and empty adjacency lists
 struct graph *createGraph(int vertices) {
     struct graph *newGraph = (struct graph *) malloc(sizeof(struct graph));
-    newGraph->vertices = vertices;
 
-    newGraph->nodes = (struct adjList *) calloc(vertices, sizeof(struct adjList));
+    // create all vertices/nodes for the graph
+    newGraph->vertices = vertices;
+    newGraph->nodes = (struct node *) malloc(vertices * sizeof(struct node));
 
     for (int i = 0; i < vertices; i++) {
-        newGraph->nodes[i].head = EMPTY_NODE;
+        struct node *nd = newGraph->nodes;
+
+        (newGraph->nodes + i)->nodeId = i;
+        (newGraph->nodes + i)->adjList = ADJ_LIST_END; // by default no adjacencies
     }
 
     return newGraph;
 }
 
-void addEdge(struct graph *g, int src, int dst) {
-    
+// add new edge to the directed graph g
+void addEdge(struct graph *g, int srcNodeId, int dstNodeId) {
+    struct adjListNode *newNode = createAdjNode(dstNodeId);
+ 
+    // add the edge to the head of node's adjacency list
+    newNode->next = g->nodes[srcNodeId].adjList;
+    g->nodes[srcNodeId].adjList = newNode;
+}
+
+void printGraph(struct graph *g) {
+    for (int i = 0; i < g->vertices; i++) {
+        struct node *v = (g->nodes + i);
+        struct adjListNode *adj = v->adjList;
+
+        printf("Adjacency list of vertex (node) %i:\n", v->nodeId);
+        printf("\thead");
+        while(adj != ADJ_LIST_END) {
+            printf(" -> %i", adj->destNode);
+            adj = adj->next;
+        }
+
+        printf("\n");
+    }
 }
 
 int main(void) {
-    // insert code here...
-    printf("Hello, World!\n");
+    struct graph *g = createGraph(5);
+    addEdge(g, 0, 1);
+    addEdge(g, 0, 4);
+    addEdge(g, 1, 2);
+    addEdge(g, 2, 3);
+    addEdge(g, 3, 4);
+    addEdge(g, 4, 5);
+ 
+    printGraph(g);
+ 
     return 0;
 }
