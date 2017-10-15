b4b634f88576c2593e329c4fdde9875db7f14bd2
Jack Zuban <jack.zuban@gmail.com>
2017-10-15 13:40:41 -0500

    robot in a grid init commit

---
+++ recursion-and-dynamic-programming/robot-in-a-grid/robot-in-a-grid/main.c
@@ -0,0 +1,12 @@
+//
+//  main.c
+//  robot-in-a-grid: find a path from the top left to the bottom right corner for a robot in a MxN grid
+//  where the robot can move only right and down, and some cells on the grid are marked as closed.
+//
+
+#include <stdio.h>
+
+int main(void) {
+
+    return 0;
+}
