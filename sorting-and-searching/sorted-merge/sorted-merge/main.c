bf2648deed6e7428502a72a2577d27373ed810de
Jack Zuban <jack.zuban@gmail.com>
2017-10-17 17:27:45 -0500

    code cleaning

--- sorting-and-searching/sorted-merge/sorted-merge/main.c
+++ sorting-and-searching/sorted-merge/sorted-merge/main.c
@@ -7,13 +7,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// merge b into a starting from the end of a
+// merge B into A starting from the end of A and B
 void merge(int a[], int b[], int aLength, int bLength) {
     int aIndex = aLength - 1;
     int bIndex = bLength - 1;
     int mergeIndex = aLength + bLength - 1;
 
-    while (bIndex >= 0) { // if bIndex < 0 exit, we don't need to copy a elements because they are already there
+    while (bIndex >= 0) { // if bIndex < 0 exit, we don't need to copy A elements because they are already there
         if (aIndex >= 0 && a[aIndex] > b[bIndex]) {
             a[mergeIndex] = a[aIndex];
             aIndex--;
@@ -35,17 +35,17 @@ void arrayPrint(int a[], int arraySize) {
 }
 
 int main(void) {
-    int a[] = {1, 2, 3, 4, 0, 0}; // the last 2 elements form a buffer for b
+    int a[] = {1, 2, 3, 4, 0, 0}; // the last 2 elements form a buffer in the A for holding B
     int b[] = {5, 6};
     int aSize = sizeof(a)/sizeof(int);
     int bSize = sizeof(b)/sizeof(int);
 
     printf("array A: ");
-    arrayPrint(a, aSize - bSize); // print only a without buffer for b
+    arrayPrint(a, aSize - bSize); // print only A array without the buffer for B
     printf("array B: ");
     arrayPrint(b, bSize);
     
-    merge(a, b, aSize - bSize, bSize); // send actual a size (without buffer)
+    merge(a, b, aSize - bSize, bSize); // send the size of A without buffer
 
     printf("array A after merging: ");
     arrayPrint(a, aSize);
