db8299f86acc79b4394a43647173e4d6c5823fbd
Jack Zuban <jack.zuban@gmail.com>
2017-10-18 14:30:55 -0500

    sorted search in no order array-like DS init commit

---
+++ sorting-and-searching/sorted-search-no-size/sorted-search-no-size/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  sorted-search-no-size: given a list structure that holds only positive int elements in a sorted order.
+//  The structure can't provide the number of its elements, but it provides getElementAt(i) method
+//  that returns the element at index i in O(1) time. If i is out of the range, the method returns -1.
+//  Find the index at which given element X occurs in the structure or -1 if it doesn't exist.
+//
+
+#include <stdio.h>
+
+int main(void) {
+
+    return 0;
+}
