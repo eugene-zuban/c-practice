1dd8acb39da9627135119e6620bb87a9d442f430
Jack Zuban <jack.zuban@gmail.com>
2017-10-17 20:07:43 -0500

    triple step: init commit

---
+++ recursion-and-dynamic-programming/triple-step/triple-step/main.c
@@ -0,0 +1,17 @@
+//
+//  main.c
+//  triple-step: count how many possible ways a child can run up a staircase with n steps
+//  jumping 1, 2 or 3 steps at a time.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int countWays(int n, int cache[]) {
+
+}
+
+int main(void) {
+
+    return 0;
+}
