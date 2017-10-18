87a7fb5da7665d99ae5db6a69179b2639eb6ca58
Jack Zuban <jack.zuban@gmail.com>
2017-10-17 23:27:18 -0500

    minimal tree init commit

---
+++ trees-and-graphs/minimal-tree/minimal-tree/main.c
@@ -0,0 +1,11 @@
+//
+//  main.c
+//  minimal-tree: create a minimal binary search tree from a sorted (INC order) array with unique integers.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+
+    return 0;
+}
