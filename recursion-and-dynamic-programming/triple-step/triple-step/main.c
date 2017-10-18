a89b1db3d32a30bf07b62156495f0d2254ae52d4
Jack Zuban <jack.zuban@gmail.com>
2017-10-17 20:25:26 -0500

    counting the number of ways using memorization

--- recursion-and-dynamic-programming/triple-step/triple-step/main.c
+++ recursion-and-dynamic-programming/triple-step/triple-step/main.c
@@ -7,11 +7,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int countWays(int n, int cache[]) {
+// count the number of ways using memorization in cache array
+unsigned long long int countWays(int n, unsigned long long int cache[]) {
+    if (n < 0) {
+        return 0;
+    }
 
+    if (n == 0) {
+        return 1;
+    }
+
+    if (cache[n] != 0) {
+        return cache[n];
+    }
+
+    cache[n] = countWays(n - 1, cache) + countWays(n - 2, cache) + countWays(n - 3, cache);
+
+    return cache[n];
 }
 
 int main(void) {
+    int steps = 0;
+    printf("Please enter number of steps: ");
+    scanf("%i", &steps);
+
+    unsigned long long int *cache = (unsigned long long int *) calloc(steps + 1, sizeof(unsigned long long int));
+    for (int i = 0; i <= steps; i++) {
+        cache[i] = 0;
+    }
+
+    printf("Number of ways is: %llu\n", countWays(steps, cache));
 
     return 0;
 }
