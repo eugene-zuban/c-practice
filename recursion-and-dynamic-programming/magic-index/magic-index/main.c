10ecf017a04d7bacd442c568c74fa7154c391e57
Jack Zuban <jack.zuban@gmail.com>
2017-10-18 12:40:09 -0500

    magic index init commit

---
+++ recursion-and-dynamic-programming/magic-index/magic-index/main.c
@@ -0,0 +1,12 @@
+//
+//  main.c
+//  magic-index: find a magic index if exists in a sorted array of unique integers.
+//  A magic index in an array A[0..n-1] is an index where A[i] = i
+//
+
+#include <stdio.h>
+
+int main(void) {
+
+    return 0;
+}
