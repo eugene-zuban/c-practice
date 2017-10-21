ad97fde51196f06f4011c4c030ff7fd1c19541ec
Jack Zuban <jack.zuban@gmail.com>
2017-10-21 12:44:23 -0500

    starse search init commit

---
+++ sorting-and-searching/sparse-search/sparse-search/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  sparse-search: find the index of a given string in an array of sorted strings
+//  that is interspersed with empty strings.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
+#include <stdbool.h>
+
+int main(void) {
+
+    return 0;
+}
