7250487a2cf9dc27bb1c67a925ccc552f4390ccc
Jack Zuban <jack.zuban@gmail.com>
2017-10-25 20:40:32 -0500

    find duplicates initial commit

---
+++ sorting-and-searching/find-duplicates/find-duplicates/main.c
@@ -0,0 +1,12 @@
+//
+//  main.c
+//  find-duplicates: find and print all the duplocates in an array from 1..N where N is max 32000
+//  Assuming that the N is not known, and only 4K of memory is available
+//
+
+#include <stdio.h>
+
+int main(void) {
+
+    return 0;
+}
