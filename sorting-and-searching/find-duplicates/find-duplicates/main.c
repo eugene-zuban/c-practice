95163e0b26510b60d1b10d91ed1894f2634aa93c
Jack Zuban <jack.zuban@gmail.com>
2017-10-26 00:43:55 -0500

    search duplicates using bitVector

--- sorting-and-searching/find-duplicates/find-duplicates/main.c
+++ sorting-and-searching/find-duplicates/find-duplicates/main.c
@@ -5,8 +5,82 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+void findDuplicates(int arr[], int arraySize);
+unsigned char *createBitVector(int vectorSize);
+bool checkNum(unsigned char *vector, int num);
+void markNum(unsigned char *vector, int num);
+
+unsigned char *createBitVector(int sizeInBits) {
+    int sizeInBytes = sizeInBits / 8 + 1;
+    unsigned char *vector = (unsigned char *) malloc(sizeInBytes * sizeof(unsigned char *));
+    if (vector == NULL) {
+        fprintf(stderr, "Memory alloc error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    for (int i = 0; i < sizeInBytes; i++) {
+        vector[i] = 0;
+    }
+
+    return vector;
+}
+
+
+// search duplicates by setting and checking each number from the array using bitvector
+void findDuplicates(int arr[], int arraySize) {
+    unsigned char *bitVector = createBitVector(arraySize);
+
+    for (int i = 0; i < arraySize; i++) {
+        int num = arr[i];
+        int num0 = num - 1;
+
+        if (checkNum(bitVector, num0)) { // if bitVector has a corresponding to num bit, the num is duplicate
+            printf("%i ", num);
+        } else { // set the bit on the bitVector related to the num
+            markNum(bitVector, num0);
+        }
+    }
+}
+
+// check if corresponding to num bit is set on bitvector
+bool checkNum(unsigned char *vector, int num) {
+    int byteNumber = num >> 3; // num / 8
+    int mask = 1 << (num & 7); // num % 8
+
+    return (vector[byteNumber] & mask) != 0;
+}
+
+// set/mark corresponding to num bit in bitvector
+void markNum(unsigned char *vector, int num) {
+    int byteNumber = num >> 3;
+    int mask = 1 << (num & 7);
+
+    vector[byteNumber] |= mask;
+}
 
 int main(void) {
+    // initialize a test array
+    int arraySize = 32000;
+    int array[arraySize];
+    for (int i = 0; i < arraySize; i++) {
+        array[i] = i + 1;
+    }
+
+    // make some duplicates
+    array[1] = 1;
+    array[4] = 4;
+    array[118] = 118;
+    array[31900] = 31900;
+    array[31999] = 31999;
+
+    // find and print the duplicates
+    printf("Print duplicates from the array: ");
+    findDuplicates(array, arraySize);
+
+    printf("\n");
 
     return 0;
 }
