b44344aa3f123d655051705b7b680455c161aa93
Jack Zuban <jack.zuban@gmail.com>
2017-10-28 22:32:17 -0500

    list of depth: init commit

---
+++ trees-and-graphs/list-of-depth/list-of-depth/main.c
@@ -0,0 +1,11 @@
+//
+//  main.c
+//  list-of-depth: create a linked list of all the nodes of a specified depth from a given binary try.
+//
+
+#include <stdio.h>
+
+int main(void) {
+
+    return 0;
+}
