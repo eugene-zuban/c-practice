3a4d3940478d0f2a93f146c07602da6d0f01a601
Jack Zuban <jack.zuban@gmail.com>
2017-10-30 21:25:36 -0500

    typo fix

--- trees-and-graphs/list-of-depth/list-of-depth/main.c
+++ trees-and-graphs/list-of-depth/list-of-depth/main.c
@@ -1,6 +1,6 @@
 //
 //  main.c
-//  list-of-depth: create a linked list of all the nodes of a specified depth from a given binary try.
+//  list-of-depth: create a linked list of all the nodes of a specified depth from a given binary tree.
 //
 
 #include <stdio.h>
---
+++ trees-and-graphs/list-of-depths/list-of-depths/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  list-of-depths
+//
+//  Created by Jack Zuban on 10/30/17.
+//  Copyright © 2017 Jack Zuban. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
