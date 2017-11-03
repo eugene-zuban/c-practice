db732e818933338b8541b7bc229fe18cdd26cc08
Jack Zuban <jack.zuban@gmail.com>
2017-11-02 22:52:59 -0500

    sorted-matrix-search init commit

---
+++ sorting-and-searching/sorted-matrix-search/sorted-matrix-search/main.c
@@ -0,0 +1,11 @@
+//
+//  main.c
+//  sorted-matrix-search: find an element in a given MxN matrix in which each row and column is sorted in ASC order.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+
+    return 0;
+}
