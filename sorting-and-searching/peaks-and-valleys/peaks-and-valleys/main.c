5762e77b61acabc68047bdcc913e490e61c949c0
Jack Zuban <jack.zuban@gmail.com>
2017-11-10 00:41:50 -0600

    peaks and valleys init commit

---
+++ sorting-and-searching/peaks-and-valleys/peaks-and-valleys/main.c
@@ -0,0 +1,12 @@
+//
+//  main.c
+//  peaks-and-valleys: sort a given array of integers into an alternating sequence of "peaks" and "valleys".
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, const char * argv[]) {
+
+    return 0;
+}
