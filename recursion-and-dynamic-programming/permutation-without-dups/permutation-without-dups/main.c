15ff99b8afc6449a44b40c690f5b9ba9618933be
Jack Zuban <jack.zuban@gmail.com>
2017-11-11 19:32:59 -0600

    implementing substring() funct

--- recursion-and-dynamic-programming/permutation-without-dups/permutation-without-dups/main.c
+++ recursion-and-dynamic-programming/permutation-without-dups/permutation-without-dups/main.c
@@ -4,8 +4,33 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char *substring(char *str, int start, int end) {
+    if (strlen(str) < end) {
+        return (char *) 0;
+    }
+
+    int subLength = end - start;
+    if (subLength <= 0) {
+        return (char *) 0;
+    }
+
+    char *substr = (char *) malloc(sizeof(char) * subLength + 1);
+    if (substr == NULL) {
+        fprintf(stderr, "Memory alloc error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    memcpy(substr, str + start, subLength);
+    *(substr + subLength) = '\0';
+
+    return substr;
+}
 
 int main(int argc, const char * argv[]) {
 
     return 0;
 }
+
