24e5fb018c2e53682ec970bcae5df35247401de3
Jack Zuban <jack.zuban@gmail.com>
2017-11-12 20:40:57 -0600

    add stack related functions

--- recursion-and-dynamic-programming/permutation-without-dups/permutation-without-dups/main.c
+++ recursion-and-dynamic-programming/permutation-without-dups/permutation-without-dups/main.c
@@ -7,14 +7,79 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct permItem {
+#define EMPTY_ITEM (stackItem *) 0
+#define EMPTY_STACK (stack *) 0
+
+typedef struct stackItem {
     char *string;
-    struct permItem *next;
+    struct stackItem *next;
+} stackItem;
+
+typedef struct stack {
+    stackItem *head;
 } stack;
 
+void memError(void) {
+    fprintf(stderr, "Memory alloc error\n");
+    exit(EXIT_FAILURE);
+}
+
+// permutation related functions
 stack *permutations(char *str);
+
+// stack related functions
+stack *createStack(void);
+stackItem *createStackItem(char *str);
+void stackAdd(stack *st, char *str);
+char *stackPop(stack *st);
+
+// strings related function
 char *substring(char *str, int start, int end);
 char *strcontat(char *str1, char *str2);
+
+// create a new linked list stack
+stack *createStack() {
+    stack *st = (stack *) malloc(sizeof(stack));
+    if (st == NULL)
+        memError();
+
+    st->head = EMPTY_ITEM;
+
+    return st;
+}
+
+// linked list item
+stackItem *createStackItem(char *str) {
+    stackItem *item = (stackItem *) malloc(sizeof(stackItem));
+    if (item == NULL) {
+        memError();
+    }
+
+    item->string = str;
+    item->next = EMPTY_ITEM;
+
+    return item;
+}
+
+void stackAdd(stack *st, char *str) {
+    stackItem *item = createStackItem(str);
+
+    item->next = st->head;
+    st->head = item;
+}
+
+char *stackPop(stack *st) {
+    if (st->head == EMPTY_ITEM) {
+        return (char *) 0;
+    }
+
+    char *string = st->head->string;
+    stackItem *item = st->head;
+    st->head = item->next;
+    free(item);
+
+    return string;
+}
 
 // return str3 = str1 + str2;
 char *strconcat(char *str1, char *str2) {
@@ -58,6 +123,10 @@ char *substring(char *str, int start, int end) {
 }
 
 int main(int argc, const char * argv[]) {
+    char *str1 = {"abc"};
+    char *str2 = {"def"};
+    stack *st = createStack();
+    int i;
 
     return 0;
 }
