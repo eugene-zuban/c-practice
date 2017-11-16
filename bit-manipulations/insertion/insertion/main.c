e2245893a132e149e808b23b35c5aae94eaab34d
Jack Zuban <jack.zuban@gmail.com>
2017-11-15 23:19:40 -0600

    insertion: initial commit

---
+++ bit-manipulations/insertion/insertion/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  insertion: insert bits from 32-bit number M into 32-bit number N into position starts from bit j and ends on bit i.
+//  example: N = 100000, M = 000111, j = 3, i = 1, result = 101110
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+int main(int argc, const char * argv[]) {
+
+    return 0;
+}
