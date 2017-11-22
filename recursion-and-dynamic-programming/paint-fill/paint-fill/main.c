018f11b83af1a23998b568330d9ae6832d3f1f41
Jack Zuban <jack.zuban@gmail.com>
2017-11-21 23:09:26 -0600

    paint fill init commit

---
+++ recursion-and-dynamic-programming/paint-fill/paint-fill/main.c
@@ -0,0 +1,12 @@
+//
+//  main.c
+//  paint-fill: given an MxN array represents a screen with pixels of different colors, a point, and a color.
+//  Fill all the surrounding area untill the color changes the original color. "Paint fill" function analogy.
+//
+
+#include <stdio.h>
+
+int main(void) {
+
+    return 0;
+}
