2a67970a305788028b123ca2171590a7c75f100c
Jack Zuban <jack.zuban@gmail.com>
2017-11-22 23:45:24 -0600

    coins: init commit

---
+++ recursion-and-dynamic-programming/coins/coins/main.c
@@ -0,0 +1,13 @@
+//
+//  main.c
+//  coins: given an infinite number of coins 25, 10, 5, and 1 cents.
+//  Compute the number of ways representing n cents.
+//
+//
+
+#include <stdio.h>
+
+int main(void) {
+
+    return 0;
+}
