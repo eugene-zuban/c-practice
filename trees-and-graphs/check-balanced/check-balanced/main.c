616a049c4eabcbdbe4fdc4c51eb7f6f8b549b5d1
Jack Zuban <jack.zuban@gmail.com>
2017-11-24 13:07:21 -0600

    check balanced tree init commit

---
+++ trees-and-graphs/check-balanced/check-balanced/main.c
@@ -0,0 +1,12 @@
+//
+//  main.c
+//  check-balanced: check if a binary tree is is balanced.
+//  A balanced tree is defined to be a tree such that the heights of the two subtrees of any node never differ by more than one.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+
+    return 0;
+}
