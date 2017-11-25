8bbfaf3c55bd653c53f8f25714e6c6cdf74c5356
Jack Zuban <jack.zuban@gmail.com>
2017-11-25 12:55:06 -0600

    validate bst: initial commit

---
+++ trees-and-graphs/validate-bst/validate-bst/main.c
@@ -0,0 +1,12 @@
+//
+//  main.c
+//  validate-bst: check if a given binary tree is a binary search tree.
+//
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+
+    return 0;
+}
