e21bea7535a766127c87a5e597e68c1724953b61
Jack Zuban <jack.zuban@gmail.com>
2017-11-26 13:14:24 -0600

    eight queens: initial commit

---
+++ recursion-and-dynamic-programming/eight-queens/eight-queens/main.c
@@ -0,0 +1,12 @@
+//
+//  main.c
+//  eight-queens: print all the ways of arangings 8 queens on an 8x8 chess board.
+//  That none of the queens shares the same row, column, or diagonal.
+//
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    return 0;
+}
