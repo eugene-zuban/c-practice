74d82d3c84b28c3280e255a77168e36969e37289
Jack Zuban <jack.zuban@gmail.com>
2018-02-17 23:40:01 -0500

    successor algorithm init commit

---
+++ trees-and-graphs/Successor/Successor/main.c
@@ -0,0 +1,10 @@
+//
+//  main.c
+//  Successor: algorithm to finding the "next" node of a given node in a BST tree.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    return 0;
+}
