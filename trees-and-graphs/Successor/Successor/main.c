22a515eec2a1467b4ebf879a4962a3e697e3bdb2
Jack Zuban <jack.zuban@gmail.com>
2018-03-25 20:31:56 -0400

    add basic tree operations

--- trees-and-graphs/Successor/Successor/main.c
+++ trees-and-graphs/Successor/Successor/main.c
@@ -3,10 +3,60 @@
 //  Successor: the algorithm for finding the "next" node of a given node in a BST tree.
 //
 
-#include <stdio.h>
+#import <stdlib.h>
+#import <stdio.h>
 
-void createBST(void);
+#define NULL_NODE (node *) 0
+
+typedef struct node {
+    int32_t key;
+    struct node *parent;
+    struct node *left;
+    struct node *right;
+} node;
+
+node *createNode(node *parent, int32_t key) {
+    node *newNode = (node *) malloc(sizeof(node));
+    if (newNode == NULL) {
+        exit(EXIT_FAILURE);
+    }
+
+    newNode->parent = parent;
+    newNode->key = key;
+    newNode->left = NULL_NODE;
+    newNode->right = NULL_NODE;
+
+    return newNode;
+}
+
+void doInOrderTraverse(node *nd, void (*operation) (node *)) {
+    if (nd == NULL_NODE) {
+        return;
+    }
+
+    doInOrderTraverse(nd->left, operation);
+    operation(nd);
+    doInOrderTraverse(nd->right, operation);
+}
+
+void printNode(node *nd) {
+    printf("%i ", nd->key);
+}
+
+void clearMemory(node *nd) {
+    free(nd);
+}
 
 int main(int argc, const char * argv[]) {
+    node *root = createNode(NULL_NODE, 40);
+    root->left = createNode(root, 30);
+    root->right = createNode(root, 50);
+    root->left->left = createNode(root->left, 20);
+
+    doInOrderTraverse(root, printNode);
+    doInOrderTraverse(root, clearMemory);
+
+    printf("\n");
+
     return 0;
 }
