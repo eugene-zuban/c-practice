be2d9ac6a71c3c7a387dbe8c171ee2c7c41ce3f5
Jack Zuban <jack.zuban@gmail.com>
2018-08-03 00:14:24 -0400

    add description

--- trees-and-graphs/build-order/build-order/main.c
+++ trees-and-graphs/build-order/build-order/main.c
@@ -1,7 +1,14 @@
+//
+//  main.c
+//  Build Order: make a build order from a given list of projects and a list of their interdependencies.
+//
+
 #include <stdio.h>
 
+typedef struct node {
+    struct node *children;
+} node;
+
 int main(int argc, const char * argv[]) {
-    printf("Hello, World!\n");
     return 0;
 }
-
