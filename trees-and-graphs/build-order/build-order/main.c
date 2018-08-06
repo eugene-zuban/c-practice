928d5852f86ce952308a3b5993d1f9d6de6df88d
Jack Zuban <jack.zuban@gmail.com>
2018-08-06 00:53:05 -0400

    add default data structures to the project

--- trees-and-graphs/build-order/build-order/main.c
+++ trees-and-graphs/build-order/build-order/main.c
@@ -4,10 +4,26 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
-typedef struct node {
-    struct node *children;
-} node;
+enum projectState {VISITING, VISITED, NOT_VISITED};
+
+typedef struct {
+    char name;
+    int incomingEdges;
+    struct project *children;
+    enum projectState state;
+} project;
+
+typedef struct {
+    struct project *node;
+    struct projectsStack *next;
+} projectsStack;
+
+projectsStack *buildOrder();
+project **projectsToBuild;
+char **projectDependencies;
 
 int main(int argc, const char * argv[]) {
     return 0;
