80c07f1a70cea986ea1200a441acfda8a960a5ca
Jack Zuban <jack.zuban@gmail.com>
2018-08-20 00:57:05 -0400

    implement main algorithm

--- trees-and-graphs/build-order/build-order/main.c
+++ trees-and-graphs/build-order/build-order/main.c
@@ -9,15 +9,16 @@
 #include <limits.h>
 
 #define NULL_PROJECT (project *) 0
+#define STACK_END (stackItem *) 0
 #define EMPTY_EDGES (project **) 0
 
-#define FIRST_SET_DEP_NUMBET 7
+#define FIRST_SET_DEP_NUMBER 7
+#define SECOND_SET_DEP_NUMBER 7
 
 enum projectState {VISITING, VISITED, NOT_VISITED};
 
 typedef struct project {
     char name;
-    int incomingEdges;
     struct project **edges;
     int numberOfEdges;
     enum projectState state;
@@ -28,10 +29,10 @@ typedef struct graph {
     int numberOfNodes;
 } graph;
 
-typedef struct {
+typedef struct stackItem {
     struct project *node;
-    struct projectsStack *next;
-} projectsStack;
+    struct stackItem *next;
+} stackItem;
 
 void memoryAllocationCheck(void *pointer) {
     if (pointer == NULL) {
@@ -54,9 +55,8 @@ project *createProject(char projectName) {
     project *p = (project *) malloc(sizeof(project));
     memoryAllocationCheck(p);
     p->name = projectName;
-    p->edges = NULL_PROJECT;
+    p->edges = EMPTY_EDGES;
     p->numberOfEdges = 0;
-    p->incomingEdges = 0;
     p->state = NOT_VISITED;
 
     return p;
@@ -99,16 +99,128 @@ void addEdgeToProjectGraph(graph *projectGraph, const char graphEdge[1]) {
     // add the edge to dependency and increase the total number of its edges
     dependency->edges[dependency->numberOfEdges] = dependent;
     dependency->numberOfEdges++;
+}
 
-    // increase the number of incoming projects on the dependent project
-    dependent->incomingEdges++;
+/**
+ * Projects stack empty checker.
+ */
+bool isStackEmpty(stackItem *stack) {
+    return stack == STACK_END;
+}
+
+/**
+ * Add a new project to the stack
+ */
+void pushToStack(stackItem **stack, project *node) {
+    stackItem *newItem = (stackItem *) malloc(1 * sizeof(stackItem));
+    memoryAllocationCheck(newItem);
+
+    newItem->node = node;
+    newItem->next = *stack;
+
+    *stack = newItem;
+}
+
+/**
+ * Pop the top project from the stack.
+ */
+project *popFromStack(stackItem **stack) {
+    stackItem *top = *stack;
+    if (isStackEmpty(top)) {
+        return NULL_PROJECT;
+    }
+
+    project *node = top->node;
+    *stack = top->next;
+    free(top);
+
+    return node;
+}
+
+/**
+ * do DFS on the project and its edges (dependent projects) to find out an order that allow to complete the projects
+ * @param node is the project to check
+ * @param buildOrder stack represents a correct build order
+ * @return true on success and false on a cycle detection
+ */
+bool projectsDfs(project *node, stackItem **buildOrder) {
+    if (node->state == VISITING) {
+        return false; // a cycle has been detected
+    }
+
+    node->state = VISITING;
+    for (int i = 0; i < node->numberOfEdges; ++i) { // go through all the projects that depend on it
+        project *dependent = node->edges[i];
+        if (dependent->state != VISITED) { // we need to look at not visited projects only
+            if (! projectsDfs(dependent, buildOrder)) {
+                return false;
+            }
+        }
+    }
+
+    pushToStack(buildOrder, node);
+    node->state = VISITED;
+
+    return true;
+}
+
+/**
+ * Take each project from the projects graph and do DFS on them.
+ * DFS allows as to find a possible order to build all the projects satisfying all the dependencies.
+ */
+stackItem *determineBuildOrder(graph *projects) {
+    stackItem *buildOrder = STACK_END;
+    for (int i = 0; i < projects->numberOfNodes; ++i) {
+        if (projects->nodes[i]->state == NOT_VISITED) {
+            if (! projectsDfs(projects->nodes[i], &buildOrder)) {
+                return STACK_END; // projects graph has a cycle
+            }
+        }
+    }
+
+    return  buildOrder;
+}
+
+void printProjects(graph *projects) {
+    printf("Projects list:");
+
+    for (int i = 0; i < projects->numberOfNodes; ++i) {
+        printf(" %c", projects->nodes[i]->name);
+    }
+}
+
+void printDependencyMap(char **array, int size) {
+    printf("\nDependency map [dependency, dependent]:");
+    for (int i = 0; i < size; ++i) {
+        printf(" [%c, %c]", array[i][0], array[i][1]);
+    }
+}
+
+void printBuildOrder(stackItem *buildOrder) {
+    printf("\nItems from the stack: ");
+    if (buildOrder == STACK_END) {
+        printf("No build order can be build, a cycle has been detected in projects interdependencies.");
+    }
+
+    while (! isStackEmpty(buildOrder)) {
+        printf("%c ", popFromStack(&buildOrder)->name);
+    }
+}
+
+/**
+ * Adds dependencies to the graph nodes to have a complete graph.
+ */
+void buildGraph(graph *projects, char **adjacencyArray, int adjacencySize) {
+    for (int i = 0; i < adjacencySize; i++) {
+        addEdgeToProjectGraph(projects, adjacencyArray[i]);
+    }
 }
 
 /**
  * Returns the adjacency list as a multi dimension array.
  */
 char **getFirstDependenciesSet() {
-    int rows = FIRST_SET_DEP_NUMBET, cols = 2;
+    int rows = FIRST_SET_DEP_NUMBER, cols = 2;
     char **dep = (char **) malloc(sizeof(char *) * rows * cols);
     for (int row = 0; row < rows; row++) {
         dep[row] = (char *) malloc(sizeof(char) * cols);
@@ -125,23 +237,53 @@ char **getFirstDependenciesSet() {
     return dep;
 }
 
-int main(int argc, const char * argv[]) {
+char **getSecondDependenciesSet() {
+    int rows = FIRST_SET_DEP_NUMBER, cols = 2;
+    char **dep = (char **) malloc(sizeof(char *) * rows * cols);
+    for (int row = 0; row < rows; row++) {
+        dep[row] = (char *) malloc(sizeof(char) * cols);
+    }
+
+    dep[0][0] = 'A'; dep[0][1] = 'B';
+    dep[1][0] = 'A'; dep[1][1] = 'E';
+    dep[2][0] = 'B'; dep[2][1] = 'C';
+    dep[3][0] = 'C'; dep[3][1] = 'F';
+    dep[4][0] = 'C'; dep[4][1] = 'G';
+    dep[5][0] = 'D'; dep[5][1] = 'A';
+    dep[6][0] = 'E'; dep[6][1] = 'D';
+
+    return dep;
+}
+
+void firstTestCaseNoCycle() {
+    printf("\nFirst case: projects with no cycle.\n");
     graph *projectsGraph = initProjectsGraph(7);
-
     char **dep = getFirstDependenciesSet();
-    for (int i = 0; i < FIRST_SET_DEP_NUMBET; i++) {
-        addEdgeToProjectGraph(projectsGraph, dep[i]);
-    }
 
-    printf("Projects list:");
-    for (int i = 0; i < 7; ++i) {
-        printf(" %c", projectsGraph->nodes[i]->name);
-    }
+    buildGraph(projectsGraph, dep, FIRST_SET_DEP_NUMBER);
+    stackItem *buildOrder = determineBuildOrder(projectsGraph);
 
-    printf("\nDependency map [dependency, dependent]:");
-    for (int i = 0; i < FIRST_SET_DEP_NUMBET; ++i) {
-        printf(" [%c, %c]", dep[0][0], dep[0][1]);
-    }
+    printProjects(projectsGraph);
+    printDependencyMap(dep, FIRST_SET_DEP_NUMBER);
+    printBuildOrder(buildOrder);
+}
+
+void secondTestCaseCycle() {
+    printf("\n\nSecond case: projects with a cycle.\n");
+    graph *projectsGraph = initProjectsGraph(7);
+    char **dep = getSecondDependenciesSet();
+
+    buildGraph(projectsGraph, dep, SECOND_SET_DEP_NUMBER);
+    stackItem *buildOrder = determineBuildOrder(projectsGraph);
+
+    printProjects(projectsGraph);
+    printDependencyMap(dep, SECOND_SET_DEP_NUMBER);
+    printBuildOrder(buildOrder);
+}
+
+int main(int argc, const char * argv[]) {
+    firstTestCaseNoCycle();
+    secondTestCaseCycle();
 
     return 0;
 }
